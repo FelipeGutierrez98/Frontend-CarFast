1af881d5d66bfda8496fad3f5759df00600bbd04
felipe gutierrez <edward.felipe.gutierrez@gmail.com>
2023-06-14 19:46:37 -0500

    creacion bycar

--- src/app/dashboard/pages/contacto/contacto.component.ts
+++ src/app/dashboard/pages/contacto/contacto.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  nombre!: string;
+  email!: string;
+  asunto!: string;
+  mensaje!: string;
 
+  constructor(private http: HttpClient) {}
+
+  enviarFormulario() {
+    const formData = {
+      nombre: this.nombre,
+      email: this.email,
+      asunto: this.asunto,
+      mensaje: this.mensaje
+    };
+
+    this.http.post('/enviar-correo', formData)
+      .subscribe(
+        () => {
+          console.log('Correo enviado');
+        },
+        error => {
+          console.error('Error al enviar el correo', error);
+        }
+      );
+  }
 }
